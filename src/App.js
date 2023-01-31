dc7042b86008ade617743b26d4b2ad44a7afefcd
Laura <lauratelqiu@gmail.com>
2023-01-31 13:39:34 -0500

    data, styling

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
-const allCategories = ['all', ... new Set(items.map((item) => item.category))]
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
 
 
 
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
+  // const [categories, setCategories] = useState(allCategories);
+  const [categories] = useState(allCategories);
 
   const filterItems = (category) => {
     if (category === "all") {
--- src/data.js
+++ src/data.js
@@ -17,14 +17,14 @@ const menu = [
     img: './images/item-2.jpeg',
     desc: 'Burger Deluxe: A classic American burger topped with all the essentials, including lettuce, tomato, pickles, onions, melted cheddar cheese, and special sauce. Served on a toasted bun and a side of fries ',
   },
-  {
-    id: 3,
-    title: 'godzilla milkshake',
-    category: 'shakes',
-    price: 6.99,
-    img: './images/item-3.jpeg',
-    desc: 'A monstrously delicious milkshake, inspired by the famous kaiju Godzilla. Made with premium vanilla ice cream, fresh milk, and a blend of sweet and savory flavors, this shake is thick, creamy, and packed with flavor. Topped with whipped cream, a drizzle of caramel sauce, and a cherry.',
-  },
+  // {
+  //   id: 3,
+  //   title: 'godzilla milkshake',
+  //   category: 'shakes',
+  //   price: 6.99,
+  //   img: './images/item-3.jpeg',
+  //   desc: 'A monstrously delicious milkshake, inspired by the famous kaiju Godzilla. Made with premium vanilla ice cream, fresh milk, and a blend of sweet and savory flavors, this shake is thick, creamy, and packed with flavor. Topped with whipped cream, a drizzle of caramel sauce, and a cherry.',
+  // },
   {
     id: 4,
     title: 'country delight',
@@ -41,14 +41,14 @@ const menu = [
     img: './images/item-5.jpeg',
     desc: 'A classic American burger topped with a sunny-side up egg, adding a touch of richness and flavor to the traditional dish. Served on a toasted bun with lettuce, tomato, pickles, onions, melted cheddar cheese, and special sauce. ',
   },
-  {
-    id: 6,
-    title: 'oreo dream',
-    category: 'shakes',
-    price: 18.99,
-    img: './images/item-6.jpeg',
-    desc: 'A creamy and indulgent milkshake made with premium vanilla ice cream, milk, and crushed Oreo cookies. Topped with whipped cream, a drizzle of chocolate sauce, and a cherry.',
-  },
+  // {
+  //   id: 6,
+  //   title: 'oreo dream',
+  //   category: 'shakes',
+  //   price: 18.99,
+  //   img: './images/item-6.jpeg',
+  //   desc: 'A creamy and indulgent milkshake made with premium vanilla ice cream, milk, and crushed Oreo cookies. Topped with whipped cream, a drizzle of chocolate sauce, and a cherry.',
+  // },
   {
     id: 7,
     title: 'bacon overflow',
@@ -65,14 +65,14 @@ const menu = [
     img: './images/item-8.jpeg',
     desc: 'A traditional American meal, featuring classic flavors and ingredients. Made with juicy burgers, crispy fries, and a cold drink, this meal is both satisfying and nostalgic.',
   },
-  {
-    id: 9,
-    title: 'quarantine buddy',
-    category: 'shakes',
-    price: 16.99,
-    img: './images/item-9.jpeg',
-    desc: 'A fun and playful milkshake that is perfect for sharing with a quarantine buddy. Made with premium vanilla ice cream, milk, and a blend of sweet and playful flavors, this shake is thick, creamy, and packed with flavor.',
-  },
+  // {
+  //   id: 9,
+  //   title: 'quarantine buddy',
+  //   category: 'shakes',
+  //   price: 16.99,
+  //   img: './images/item-9.jpeg',
+  //   desc: 'A fun and playful milkshake that is perfect for sharing with a quarantine buddy. Made with premium vanilla ice cream, milk, and a blend of sweet and playful flavors, this shake is thick, creamy, and packed with flavor.',
+  // },
   {
     id: 10,
     title: 'steak',
@@ -80,7 +80,8 @@ const menu = [
     price: 36.99,
     img: './images/item-10.jpeg',
     desc: 'A juicy and flavorful cut of beef, cooked to perfection and served with a side of your choice. Made with high-quality, tender cuts of beef, this steak is grilled to perfection and topped with a pat of butter for added richness.',
-  },
+  }
+
 
 
 ];
