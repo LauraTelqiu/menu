414ab74648b3ddf72e8a70d27254dc206c852d6b
Laura <lauratelqiu@gmail.com>
2023-01-31 09:54:37 -0500

    lunch, shakes

--- src/Categories.js
+++ src/Categories.js
@@ -5,6 +5,8 @@ function Categories({ filterItems }) {
   return (
     <div className='btn-container'>
       <button className='filter-btn' onClick={() => filterItems('breakfast')}>Breakfast</button>
+      <button className='filter-btn' onClick={() => filterItems('lunch')}>Lunch</button>
+      <button className='filter-btn' onClick={() => filterItems('shakes')}>Shakes</button>
     </div>
   )
 }
