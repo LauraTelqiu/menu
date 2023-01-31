0d759efdd5f377840999f72666b909df61669657
Laura <lauratelqiu@gmail.com>
2023-01-31 11:49:25 -0500

    data

--- src/data.js
+++ src/data.js
@@ -1,3 +1,5 @@
+
+
 const menu = [
   {
     id: 1,
@@ -5,7 +7,7 @@ const menu = [
     category: 'breakfast',
     price: 15.99,
     img: './images/item-1.jpeg',
-    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    desc: 'Fluffy and tender buttermilk pancakes served with a generous pat of butter and syrup of your choice.',
   },
   {
     id: 2,
@@ -13,7 +15,7 @@ const menu = [
     category: 'lunch',
     price: 13.99,
     img: './images/item-2.jpeg',
-    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    desc: 'Burger Deluxe: A classic American burger topped with all the essentials, including lettuce, tomato, pickles, onions, melted cheddar cheese, and special sauce. Served on a toasted bun and a side of fries ',
   },
   {
     id: 3,
@@ -21,7 +23,7 @@ const menu = [
     category: 'shakes',
     price: 6.99,
     img: './images/item-3.jpeg',
-    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    desc: 'A monstrously delicious milkshake, inspired by the famous kaiju Godzilla. Made with premium vanilla ice cream, fresh milk, and a blend of sweet and savory flavors, this shake is thick, creamy, and packed with flavor. Topped with whipped cream, a drizzle of caramel sauce, and a cherry.',
   },
   {
     id: 4,
@@ -29,7 +31,7 @@ const menu = [
     category: 'breakfast',
     price: 20.99,
     img: './images/item-4.jpeg',
-    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    desc: ' A hearty and wholesome meal, inspired by the flavors of the countryside. Made with a combination of fresh ingredients, this dish features savory meats, crisp vegetables, and fluffy potatoes, all cooked to perfection. ',
   },
   {
     id: 5,
@@ -37,7 +39,7 @@ const menu = [
     category: 'lunch',
     price: 22.99,
     img: './images/item-5.jpeg',
-    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    desc: 'A classic American burger topped with a sunny-side up egg, adding a touch of richness and flavor to the traditional dish. Served on a toasted bun with lettuce, tomato, pickles, onions, melted cheddar cheese, and special sauce. ',
   },
   {
     id: 6,
@@ -45,7 +47,7 @@ const menu = [
     category: 'shakes',
     price: 18.99,
     img: './images/item-6.jpeg',
-    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    desc: 'A creamy and indulgent milkshake made with premium vanilla ice cream, milk, and crushed Oreo cookies. Topped with whipped cream, a drizzle of chocolate sauce, and a cherry.',
   },
   {
     id: 7,
@@ -53,7 +55,7 @@ const menu = [
     category: 'breakfast',
     price: 8.99,
     img: './images/item-7.jpeg',
-    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+    desc: 'dish that combines savory bacon with fluffy eggs and crispy potatoes. Made with a generous serving of crispy bacon, fluffy scrambled eggs, and crispy hash browns, this dish is filling, flavorful, and packed with protein.',
   },
   {
     id: 8,
@@ -61,7 +63,7 @@ const menu = [
     category: 'lunch',
     price: 12.99,
     img: './images/item-8.jpeg',
-    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+    desc: 'A traditional American meal, featuring classic flavors and ingredients. Made with juicy burgers, crispy fries, and a cold drink, this meal is both satisfying and nostalgic.',
   },
   {
     id: 9,
@@ -69,7 +71,17 @@ const menu = [
     category: 'shakes',
     price: 16.99,
     img: './images/item-9.jpeg',
-    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+    desc: 'A fun and playful milkshake that is perfect for sharing with a quarantine buddy. Made with premium vanilla ice cream, milk, and a blend of sweet and playful flavors, this shake is thick, creamy, and packed with flavor.',
   },
+  {
+    id: 10,
+    title: 'steak',
+    category: 'dinner',
+    price: 36.99,
+    img: './images/item-10.jpeg',
+    desc: 'A juicy and flavorful cut of beef, cooked to perfection and served with a side of your choice. Made with high-quality, tender cuts of beef, this steak is grilled to perfection and topped with a pat of butter for added richness.',
+  },
+
+
 ];
 export default menu;
