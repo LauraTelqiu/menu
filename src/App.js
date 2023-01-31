c511037468fa10c23bbf61044dc4a668b28598ce
Laura <lauratelqiu@gmail.com>
2023-01-30 20:46:21 -0500

    menut items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-
+import { useState } from "react";
+import Menu from './Menu'
+import Categories from './Categories'
+import items from './data'
 function App() {
+  const [menuItems, setMenuItems] = useState(items)
+  const [categories, setCategories] = useState([])
   return (
-    <>App</>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   )
 
 
